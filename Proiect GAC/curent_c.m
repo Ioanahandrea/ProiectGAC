32f8d980885cb3817e3d2e90854e757350425c05
Ioanahandrea <93031174+Ioanahandrea@users.noreply.github.com>
2023-04-24 23:39:11 +0300

    Add files via upload

---
+++ Proiect GAC/curent_c.m
@@ -0,0 +1,11 @@
+clear all;
+close all;
+
+R1=500;
+R2=500;
+R3=500;
+beta=100;
+Val=15;
+
+
+curent_continuu(R1,R2,R3,beta,Val)
---
+++ Proiect GAC/semnal_m.m
@@ -0,0 +1,12 @@
+clear all;
+close all;
+A=1;
+f=500;
+n=5;
+R1=500;
+R2=500;
+R3=500;
+beta=100;
+Val=15;
+x=1;
+semnal_mic(A,f,n,R1,R2,R3,beta,Val,x)
